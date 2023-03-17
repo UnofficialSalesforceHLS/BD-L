5cb0d1b0dba1ddb1efe8f32e21c9a5fb97c6573b
Demen Selcan <demen.selcan@gmail.com>
2023-03-17 18:06:29 -0400

    -created user and orgInit

---
+++ orgInit.sh
@@ -0,0 +1,10 @@
+sfdx force:org:create -f config/project-scratch-def.json -s -d 1 -w 15
+sfdx force:source:push --forceoverwrite --loglevel fatal --wait 30
+sfdx force:user:create -a BDLAdmin -f users/hc_patientcoordination.json
+sfdx force:user:permset:assign -n BDL_BDL_Admin
+sfdx shane:user:password:set -g User -l User -p iLoveSalesforceBDL
+sfdx shane:user:password:set -g BDL -l Admin -p iLoveSalesforceBDL
+sfdx force:org:open
+
+
+
