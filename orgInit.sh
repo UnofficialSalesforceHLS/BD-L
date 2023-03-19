8799b3f078ec81f950582f39af6f5e7ed67915d6
Demen Selcan <demen.selcan@gmail.com>
2023-03-19 21:10:51 +0100

    -updated demo data and scripts

--- orgInit.sh
+++ orgInit.sh
@@ -2,9 +2,7 @@ sfdx force:org:create -f config/project-scratch-def.json -s -d 1 -w 15
 sfdx force:source:push --forceoverwrite --loglevel fatal --wait 30
 sfdx force:user:create -a BDLAdmin -f users/bdl_admin.json
 sfdx force:user:permset:assign -n BDL_BDL_Admin
-sfdx force:data:tree:import -f data/Account.json
-sfdx force:data:tree:import -f data/Contact.json
-sfdx force:data:tree:import -f data/Asset.json
+sfdx force:data:tree:import -p ./data/BDL_Demo_Records-plan.json
 sfdx force:data:tree:import -f data/CollaborationGroup.json
 sfdx shane:user:password:set -g User -l User -p iLoveSalesforceBDL2023
 sfdx shane:user:password:set -g BDL -l Admin -p iLoveSalesforceBDL2023
