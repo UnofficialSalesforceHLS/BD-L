d7befa2ff6b695402498130d16432f2a9496fecf
Demen Selcan <demen.selcan@gmail.com>
2023-03-24 09:31:01 +0100

    -updated data import in orgInit

--- orgInit.sh
+++ orgInit.sh
@@ -2,12 +2,7 @@ sfdx force:org:create -f config/project-scratch-def.json -s -d 1 -w 15
 sfdx force:source:push --forceoverwrite --loglevel fatal --wait 30
 sfdx force:user:create -a BDLAdmin -f users/bdl_admin.json
 sfdx force:user:permset:assign -n BDL_BDL_Admin
-#sfdx force:data:tree:import -p ./data/BDL_Demo_Records-plan.json
-sfdx texei:data:import --inputdir ./data/
-#sfdx force:data:tree:import -f data/CollaborationGroup.json
+sfdx texei:data:import --inputdir /data
 sfdx shane:user:password:set -g User -l User -p iLoveSalesforceBDL2023
 sfdx shane:user:password:set -g BDL -l Admin -p iLoveSalesforceBDL2023
 sfdx force:org:open
-
-
-
