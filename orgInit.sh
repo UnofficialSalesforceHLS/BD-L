d2a2191e4990b47d1860b6ad1a0161598bc8e3ef
Demen Selcan <demen.selcan@gmail.com>
2023-03-25 15:35:22 +0100

    -added dev dependency for texei

--- orgInit.sh
+++ orgInit.sh
@@ -2,7 +2,7 @@ sfdx force:org:create -f config/project-scratch-def.json -s -d 1 -w 15
 sfdx force:source:push --forceoverwrite --loglevel fatal --wait 30
 sfdx force:user:create -a BDLAdmin -f users/bdl_admin.json
 sfdx force:user:permset:assign -n BDL_BDL_Admin
-sfdx texei:data:import --inputdir /data
+sfdx texei:data:import --inputdir ./data
 sfdx shane:user:password:set -g User -l User -p iLoveSalesforceBDL2023
 sfdx shane:user:password:set -g BDL -l Admin -p iLoveSalesforceBDL2023
 sfdx force:org:open
